59c86eb4b9e7b92b55eacb89b714a3a2afcb91c5
Daniel Clarke <d.clarke@bearcloudgames.com>
2019-04-27 17:06:53 -0400

    Fixing ui layout

--- Ludum Dare 44/Assets/Scripts/GameManager.cs
+++ Ludum Dare 44/Assets/Scripts/GameManager.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager instance;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
     }
 }
