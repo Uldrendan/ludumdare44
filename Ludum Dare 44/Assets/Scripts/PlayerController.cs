c0fac3298638e5e7b258813733b7713fca00cbc8
Daniel Clarke <d.clarke@bearcloudgames.com>
2019-04-28 16:35:48 -0400

    Adding blink functionality

--- Ludum Dare 44/Assets/Scripts/PlayerController.cs
+++ Ludum Dare 44/Assets/Scripts/PlayerController.cs
@@ -21,6 +21,10 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Blink();
+        }
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.55f), Vector2.down, 0.1f);
         Debug.DrawRay(new Vector2(transform.position.x,transform.position.y-0.5f), Vector2.down, Color.red);
         if (hit.collider != null && hit.collider.gameObject.tag == "Ground")
@@ -34,6 +38,19 @@ public class PlayerController : MonoBehaviour
         else if(move.x > 0)
             _sr.flipX = false;
         _anim.SetFloat("WalkSpeed", Mathf.Abs(move.x));
-        _rb.velocity = move;        
+        _rb.velocity = move;  
+    }
+
+    void Blink()
+    {
+        Vector3 blinkDir = Vector2.zero;
+        if (Input.GetAxis("Horizontal") > 0)
+            blinkDir = Vector2.right;
+        else if (Input.GetAxis("Horizontal") < 0)
+            blinkDir = Vector2.left;
+        else
+            blinkDir = Vector2.down;
+
+        transform.position += blinkDir;
     }
 }
