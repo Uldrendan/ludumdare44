bfa043d39be29269f35848080b2c721085831240
Daniel Clarke <d.clarke@bearcloudgames.com>
2019-04-28 16:46:06 -0400

    Prototype for shifting

--- Ludum Dare 44/Assets/Scripts/PlayerController.cs
+++ Ludum Dare 44/Assets/Scripts/PlayerController.cs
@@ -21,10 +21,6 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            Blink();
-        }
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.55f), Vector2.down, 0.1f);
         Debug.DrawRay(new Vector2(transform.position.x,transform.position.y-0.5f), Vector2.down, Color.red);
         if (hit.collider != null && hit.collider.gameObject.tag == "Ground")
@@ -38,19 +34,29 @@ public class PlayerController : MonoBehaviour
         else if(move.x > 0)
             _sr.flipX = false;
         _anim.SetFloat("WalkSpeed", Mathf.Abs(move.x));
-        _rb.velocity = move;  
+        _rb.velocity = move;
+
+        if (Input.GetKeyDown(KeyCode.B))
+            Blink();
+        if (Input.GetKeyDown(KeyCode.V))
+            Shift();
     }
 
     void Blink()
     {
-        Vector3 blinkDir = Vector2.zero;
-        if (Input.GetAxis("Horizontal") > 0)
-            blinkDir = Vector2.right;
-        else if (Input.GetAxis("Horizontal") < 0)
-            blinkDir = Vector2.left;
-        else
-            blinkDir = Vector2.down;
+        Vector3 blinkDir = Vector2.down;
 
         transform.position += blinkDir;
+    }
+
+    void Shift()
+    {
+        Vector3 shiftDir = Vector3.zero;
+
+        if (!_sr.flipX)
+            shiftDir = Vector3.right * 3;
+        else
+            shiftDir = Vector3.left * 3;
+        transform.position += shiftDir;
     }
 }
